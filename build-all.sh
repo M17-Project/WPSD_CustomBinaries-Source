37a17605b29c29e6460ef48ff1a61d737620d097
Chipster <go@w.ay>
2023-08-10 17:11:46 -0500

    update mmdvmhost with upsgtream changes, fix libgps deps. for gws

--- build-all.sh
+++ build-all.sh
@@ -8,30 +8,28 @@ if [ "$#" -ne 1 ] || ( [ "$1" != "gpsd" ] && [ "$1" != "nogpsd" ] ); then
   exit 1
 fi
 
-# Set the branch name based on the argument passed to the script
-if [ "$1" == "gpsd" ]; then
-  BRANCH="gpsd"
-elif [ "$1" == "nogpsd" ]; then
-  BRANCH="master"
-fi
+## Set the branch name based on the argument passed to the script
+#if [ "$1" == "gpsd" ]; then
+#  BRANCH="gpsd"
+#elif [ "$1" == "nogpsd" ]; then
+#  BRANCH="master"
+#fi
 
 SRC_DIR=$HOME/dev/WPSD_CustomBinaries-Source
 DEST_DIR=$HOME/dev/W0CHP-PiStar-bin
 
-# Switch to the respective branch in the DEST_DIR repository
-if ! git -C "$DEST_DIR" checkout "$BRANCH"; then
-  echo "Error: Failed to switch to branch '$BRANCH' in '$DEST_DIR' repository."
-  exit 1
-fi
+## Switch to the respective branch in the DEST_DIR repository
+#if ! git -C "$DEST_DIR" checkout "$BRANCH"; then
+#  echo "Error: Failed to switch to branch '$BRANCH' in '$DEST_DIR' repository."
+#  exit 1
+#fi
 
 # now, select the proper Makefiles for the OSs and gatweays that use 'libgps'
 if [ "$1" == "gpsd" ]; then
   MAKEFILE="Makefile"
 elif [ "$1" == "nogpsd" ]; then
-  MAKEFILE="Makefile.buster" # no libgps, sunsetting
+  MAKEFILE="Makefile.buster" # no libgps, sunsetted now that libgps28 is in buster backports
 fi
-
-# continue with the rest of the script
 
 cd $SRC_DIR/APRSGateway && make clean && make -j$(nproc) && make install && make clean
 cd $SRC_DIR/MMDVMHost && make clean && make -j$(nproc) && make install && make clean && \
