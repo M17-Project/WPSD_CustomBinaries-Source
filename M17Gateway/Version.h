95841cac30be62c9b7446c2514528b26bcc081c0
Chipster <go@w.ay>
2024-01-26 03:25:33 -0600

    update dmrgw

--- M17Gateway/Version.h
+++ M17Gateway/Version.h
@@ -19,6 +19,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20240109_WPSD";
+const char* VERSION = "20240126_WPSD";
 
 #endif
--- MMDVM_CM/USRP2DMR/Version.h
+++ MMDVM_CM/USRP2DMR/Version.h
@@ -20,6 +20,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20240109_WPSD";
+const char* VERSION = "20240126_WPSD";
 
 #endif
