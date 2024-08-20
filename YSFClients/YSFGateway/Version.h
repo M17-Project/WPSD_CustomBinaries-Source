96c4e5138d53ad39514adbac99bfbd487d937b38
Chipster <go@w.ay>
2024-08-20 04:28:41 -0500

    cleanup

--- YSFClients/YSFGateway/Version.h
+++ YSFClients/YSFGateway/Version.h
@@ -19,6 +19,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20240819_WPSD";
+const char* VERSION = "20240820_WPSD";
 
 #endif
--- build-all.sh
+++ build-all.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-cwd=$(pwd)
-
 # Check for the correct arguments
 if [ "$#" -ne 1 ] || ( [ "$1" != "gpsd" ] && [ "$1" != "nogpsd" ] ); then
   echo "Usage: $0 {gpsd|nogpsd}"
   exit 1
 fi
+
+echo "cleaning..."
+find . -type d -print0 | xargs -0 -I {} sh -c 'cd "{}" && make clean > /dev/null 2>&1'
+
+cwd=$(pwd)
 
 ## Set the branch name based on the argument passed to the script
 #if [ "$1" == "gpsd" ]; then
@@ -48,10 +51,10 @@ cd $SRC_DIR/MMDVM_CM/DMR2NXDN && make clean && make -j$(nproc) && make install &
 cd $SRC_DIR/MMDVM_CM/YSF2DMR && make clean && make -j$(nproc) && make install && make clean
 cd $SRC_DIR/MMDVM_CM/YSF2P25 && make clean && make -j$(nproc) && make install && make clean
 cd $SRC_DIR/MMDVM_CM/YSF2NXDN && make clean && make -j$(nproc) && make install && make clean
-cd $SRC_DIR/NXDNClients/NXDNGateway && make -f $MAKEFILE clean && make -f $MAKEFILE -j$(nproc) && make -f $MAKEFILE install && make -f $MAKEFILE clean
-cd $SRC_DIR/NXDNClients/NXDNParrot && make clean && make -j$(nproc) && make clean
-cd $SRC_DIR/NXDNClients/NXDNGateway && make -f $MAKEFILE clean && make -f $MAKEFILE -j$(nproc) && make -f $MAKEFILE install && make -f $MAKEFILE clean
-cd $SRC_DIR/P25Clients && make clean && make -j$(nproc) && make install && make clean
+cd $SRC_DIR/NXDNClients/NXDNGateway && make -f $MAKEFILE clean && make -f $MAKEFILE -j$(nproc) all && make -f $MAKEFILE install && make -f $MAKEFILE clean
+cd $SRC_DIR/NXDNClients/NXDNParrot && make clean && make -j$(nproc) all && make clean
+cd $SRC_DIR/P25Clients/P25Gateway && make -f $MAKEFILE clean && make -f $MAKEFILE -j$(nproc) all && make -f $MAKEFILE install && make -f $MAKEFILE clean
+cd $SRC_DIR/P25Clients/P25Parrot && make clean && make -j$(nproc) all && make install && make clean
 cd $SRC_DIR/YSFClients/YSFGateway && make -f $MAKEFILE clean && make -f $MAKEFILE -j$(nproc) && make -f $MAKEFILE install && make -f $MAKEFILE clean
 cd $SRC_DIR/YSFClients/DGIdGateway && make -f $MAKEFILE clean && make -f $MAKEFILE -j$(nproc) && make -f $MAKEFILE install && make -f $MAKEFILE clean
 cd $SRC_DIR/YSFClients/YSFParrot && make clean && make -j$(nproc) && make install && make clean
