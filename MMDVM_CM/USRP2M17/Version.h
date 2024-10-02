06921b85c352a9b75028411f35e5023cf6dd04af
Chipster <go@w.ay>
2024-10-02 01:43:43 -0500

    update with latest MMDVMHost upstr. changes

--- MMDVM_CM/USRP2M17/Version.h
+++ MMDVM_CM/USRP2M17/Version.h
@@ -20,6 +20,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20240924_WPSD";
+const char* VERSION = "20241002_WPSD";
 
 #endif
--- ircDDBGateway/Common/Version.h
+++ ircDDBGateway/Common/Version.h
@@ -24,9 +24,9 @@
 const wxString VENDOR_NAME = wxT("W0CHP");
 
 #if defined(__WXDEBUG__)
-const wxString VERSION = wxT("20240924_WPSD");
+const wxString VERSION = wxT("20241002_WPSD");
 #else
-const wxString VERSION = wxT("20240924_WPSD");
+const wxString VERSION = wxT("20241002_WPSD");
 #endif
 
 #endif
