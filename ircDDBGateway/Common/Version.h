afa68ac4917d3130c16ecd4fc86214f611598c50
Chipster <go@w.ay>
2024-10-05 05:19:12 -0500

    revert

--- ircDDBGateway/Common/Version.h
+++ ircDDBGateway/Common/Version.h
@@ -24,9 +24,9 @@
 const wxString VENDOR_NAME = wxT("W0CHP");
 
 #if defined(__WXDEBUG__)
-const wxString VERSION = wxT("20241002_WPSD");
+const wxString VERSION = wxT("20241005_WPSD");
 #else
-const wxString VERSION = wxT("20241002_WPSD");
+const wxString VERSION = wxT("20241005_WPSD");
 #endif
 
 #endif
