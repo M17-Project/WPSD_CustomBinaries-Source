ef2648d2c06eb4835c48525e8151b0487c3b1c2e
Chipster <go@w.ay>
2024-11-01 02:08:46 -0500

    m17gw uopdate from upstr.

--- MMDVM_CM/USRP2YSF/Version.h
+++ MMDVM_CM/USRP2YSF/Version.h
@@ -20,6 +20,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20241005_WPSD";
+const char* VERSION = "20241101_WPSD";
 
 #endif
--- ircDDBGateway/Common/Version.h
+++ ircDDBGateway/Common/Version.h
@@ -24,9 +24,9 @@
 const wxString VENDOR_NAME = wxT("W0CHP");
 
 #if defined(__WXDEBUG__)
-const wxString VERSION = wxT("20241005_WPSD");
+const wxString VERSION = wxT("20241101_WPSD");
 #else
-const wxString VERSION = wxT("20241005_WPSD");
+const wxString VERSION = wxT("20241101_WPSD");
 #endif
 
 #endif
