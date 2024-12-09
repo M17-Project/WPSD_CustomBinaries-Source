f215ee28b5f96b22af2adcc7d18be353b24538d8
Chipster <go@w.ay>
2024-12-09 14:57:44 -0600

    rebuild

--- MMDVM_CM/DMR2YSF/Version.h
+++ MMDVM_CM/DMR2YSF/Version.h
@@ -20,6 +20,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20241203_WPSD";
+const char* VERSION = "20241209_WPSD";
 
 #endif
--- MMDVM_CM/NXDN2DMR/Version.h
+++ MMDVM_CM/NXDN2DMR/Version.h
@@ -20,7 +20,7 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20241203_WPSD";
+const char* VERSION = "20241209_WPSD";
 
 #endif
 
