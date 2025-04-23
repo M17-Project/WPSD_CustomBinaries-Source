008d04dcb8227224c24f63a8687c8335db2453b2
Chipster <go@w.ay>
2025-04-23 07:28:50 -0500

    stage2

--- YSFClients/DGIdGateway/APRSWriter.cpp
+++ YSFClients/DGIdGateway/APRSWriter.cpp
@@ -281,7 +281,7 @@ void CAPRSWriter::sendIdFrameFixed()
                 symbol.append("D&");
 
 	char output[500U];
-	::sprintf(output, "%s>APDG03,TCPIP*,qAC,%s:!%s%c%c%s%c%c/A=%06.0f%s %s\r\n",
+	::sprintf(output, "%s>APDG03,TCPIP*,qAC,%s:!%s%c%c%s%c%c/A=%06.0f%s %s\r\n%s>APDG03:>Powered by WPSD (https://wpsd.radio)\r\n",
 		m_callsign.c_str(), server.c_str(),
 		lat, (m_latitude < 0.0F)  ? 'S' : 'N', symbol[0],
 		lon, (m_longitude < 0.0F) ? 'W' : 'E', symbol[1],
