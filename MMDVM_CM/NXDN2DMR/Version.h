16d3d7ebdc564d9b7b55522ad19fdd13f1f2fcf6
Chipster <go@w.ay>
2025-04-23 06:44:14 -0500

    stage

--- MMDVM_CM/NXDN2DMR/Version.h
+++ MMDVM_CM/NXDN2DMR/Version.h
@@ -20,7 +20,7 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20250420_WPSD";
+const char* VERSION = "20250423_WPSD";
 
 #endif
 
--- MMDVM_CM/YSF2NXDN/Version.h
+++ MMDVM_CM/YSF2NXDN/Version.h
@@ -20,6 +20,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20250420_WPSD";
+const char* VERSION = "20250423_WPSD";
 
 #endif
